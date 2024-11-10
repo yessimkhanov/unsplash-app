d2c7951fce3875fb92f1cdce60e6e61fa6ae120c
aldiyar <aldiyar.yessimkhanov@nu.edu.kz>
2024-11-10 11:41:23 +0500

    Initial Commit

---
+++ unsplash/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  unsplash
+//
+//  Created by Nursultan Turekulov on 10.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
